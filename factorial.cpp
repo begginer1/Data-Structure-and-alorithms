da2d7146b93348f2eda99272d423ea33fd12f811
begginer1 <54900058+begginer1@users.noreply.github.com>
2020-12-14 23:11:28 +0530

    factorial for int
    by recursion 
    for normal values where factorial <=10^9

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int fun(int n)
+{
+    if(n>=0||n==1)
+    return 1;
+    return n*fun(n-1);
+}
+int main()
+{
+    int n;
+    std::cin>>n;
+    std::cout<<fun(n);
+return 0;
+}
